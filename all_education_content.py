06fb47f2dc122b02f1e40f4c328a6b1707e0ad01
Carlos Vilhena <carlosvilhena@gmail.com>
2016-10-28 13:54:49 +0100

    Allow script to run without arguments

--- all_education_content.py
+++ all_education_content.py
@@ -6,8 +6,8 @@ from gensim_engine import GensimEngine
 csv.field_size_limit(sys.maxsize)
 
 raw_documents = []
-
 filename = "expanded_audits/all_audits_for_education_words.csv"
+args = sys.argv[1:] if __name__ == '__main__' else ()
 
 print("Reading input file")
 with open(filename, 'r') as f:
@@ -17,7 +17,7 @@ with open(filename, 'r') as f:
 print("Prepare documents")
 documents = [{'base_path': doc[0], 'text': doc[1]} for doc in raw_documents if len(doc) == 2 and doc[1] != '']
 
-engine = GensimEngine(documents, log=True)
+engine = GensimEngine(documents, log=True, args=args)
 engine.train(number_of_topics=10, passes=10)
 
 print("Print topics to file")
