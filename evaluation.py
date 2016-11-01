69b0c59911eb0cb6a399aedffbf69da312ef755d
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2016-11-01 14:42:36 +0000

    Remove small value added to cm2 and comment
    * Added additional comments around KL calculation
    * Removed addition of very small number to cm2 matrix (only required for very small corpuses)

--- evaluation.py
+++ evaluation.py
@@ -8,13 +8,20 @@ def relative_entropy(p, q):
     """
     A measure of the difference between two probability distributions P and Q
     https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
+
+    This is a measure of the amount of information that is lost when predicting
+    probability distribution P based on a model Q.  p(P||Q) =/= p(Q||P)
+    i.e. non-symmetric, so calculate entropy both ways, and sum to calculate.
     """
     return np.sum([stats.entropy(p, q), stats.entropy(q, p)])
 
 
 class ModelEvaluator(object):
     """
-    Evaluate LDA models using Symmetric KL divergence.
+    Evaluate LDA models using Symmetric Kullback-LeiblerL divergence.
+    This implementation is explained here https://archive.is/KBGwt, 
+    Gist: https://gist.github.com/cigrainger/62910e58db46b7397de2
+    Based on Arun et al: http://link.springer.com/chapter/10.1007/978-3-642-13657-3_43
     """
     def __init__(self, corpus):
         self.corpus = corpus
@@ -26,16 +33,22 @@ class ModelEvaluator(object):
         Evaluate the model. Higher is better.
         """
         # numpy.exp(dirichlet_expectation(model.state.sstats))
+        # .expElogbeta gives P(word|topic)
         m1 = model.expElogbeta
 
-        # Singular Value Decomposition
+        # Singular Value Decomposition to create the sigma matrix
+        # A Diagonal m x n matrix 
         U, cm1, V = np.linalg.svd(m1)
 
         topics = model[self.corpus]
+        # corpus2dense converts sparse matrix to dense
         document_term_matrix = matutils.corpus2dense(topics, model.num_topics).transpose()
-
+        
         cm2 = self.document_term_counts.dot(document_term_matrix)
-        cm2 += 0.0001
+        
+        # Adding negligible values to cm2 is required on very
+        # corpuses, so we can ignore here
+        #cm2 += 0.0001
 
         cm2norm = np.linalg.norm(self.document_term_counts)
         cm2 = cm2/cm2norm
